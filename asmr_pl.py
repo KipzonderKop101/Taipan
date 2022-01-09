ee8df3dc0b69caed83f0c5f56488df82352a3254
KipZonderKop101 <80620930+KipzonderKop101@users.noreply.github.com>
2022-01-09 21:38:24 +0100

    Add files via upload

---
+++ asmr_pl.py
@@ -0,0 +1,66 @@
+##########################################
+# TOKENS
+##########################################
+
+TT_INT      = 'TT_INT'
+TT_FLOAT    = 'TT_FLOAT'
+TT_PLUS     = 'TT_PLUS'
+TT_MINUS    = 'TT_MINUS'
+TT_MUL      = 'TT_MUL'
+TT_DIV      = 'TT_DIV'
+TT_LPAREN   = 'TT_LPAREN'
+TT_RPAREN   = 'TT_RPAREN'
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+##########################################
+# LEXER 
+##########################################
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[pos] if self.pos < len(self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char == "+":
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == "-":
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == "*":
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char == "/":
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char == "(":
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ")":
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+
+        return tokens
+
+    def make_numbers(self):
+        num_str = ''
