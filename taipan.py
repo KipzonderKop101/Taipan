17f04b1c995e374dc1d59e62613a4d49b08e0eac
KipZonderKop101 <80620930+KipzonderKop101@users.noreply.github.com>
2022-01-10 13:30:38 +0100

    Fixed division by 0
    Divisions by 0 will no longer crash Taipan

--- taipan.py
+++ taipan.py
@@ -311,6 +311,22 @@ class Parser:
 # RUNTIME RESULT
 #######################################
 
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+    
+    def register(self, res):
+        if res.error: self.error = res.error
+        return res.value
+    
+    def success(self, value):
+        self.value = value
+        return self
+    
+    def failure(self, error):
+        self.error = error
+        return self
 
 #######################################
 # VALUES
@@ -328,19 +344,24 @@ class Number:
 	
 	def added_to(self, other):
 		if isinstance(other, Number):
-			return Number(self.value + other.value)
+			return Number(self.value + other.value), None
 		
 	def subbed_by(self, other):
 		if isinstance(other, Number):
-			return Number(self.value - other.value)
+			return Number(self.value - other.value), None
 
 	def multed_by(self, other):
 		if isinstance(other, Number):
-			return Number(self.value * other.value)
+			return Number(self.value * other.value), None
 	
 	def dived_by(self, other):
 		if isinstance(other, Number):
-			return Number(self.value / other.value)
+			if other.value == 0:        
+				return None, RTError(
+					other.pos_start, other.pos_end,
+					'Division by zero'
+				)
+		return Number(self.value / other.value), None
 	
 	def __repr__(self):
 		return str(self.value)
@@ -361,30 +382,46 @@ class Interpreter:
   #######################################
 
 	def visit_NumberNode(self, node):
-		return Number(node.tok.value).set_pos(node.pos_start, node.pos_end)
-    
+		return RTResult().success(
+            Number(node.tok.value).set_pos(node.pos_start, node.pos_end)
+        )
+
 	def visit_BinOpNode(self, node):
-		left = self.visit(node.left_node)
-		right = self.visit(node.right_node)
+		res = RTResult()
+		left = res.register(self.visit(node.left_node))
+		if res.error: return res
+		right = res.register(self.visit(node.right_node))
+		if res.error: return res
 	
 		if node.op_tok.type == TT_PLUS:
-			result = left.added_to(right)
+			result, error = left.added_to(right)
 		elif node.op_tok.type == TT_MINUS:
-			result = left.subbed_by(right)
+			result, error = left.subbed_by(right)
 		elif node.op_tok.type == TT_MUL:
-			result = left.multed_by(right)
+			result, error = left.multed_by(right)
 		elif node.op_tok.type == TT_DIV:
-			result = left.dived_by(right)
+			result, error = left.dived_by(right)
 		
-		return result.set_pos(node.pos_start, node.pos_end)
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(result.set_pos(node.pos_start, node.pos_end))
     
 	def visit_UnaryOpNode(self, node):
-		number = self.visit(node.node)
+		res = RTResult()
+		number = res.register(self.visit(node.node))
+		if res.error: return res
+
+		error = None
 
 		if node.op_tok.type == TT_MINUS:
-			number = number.multed_by(Number(-1))
-		
-		return number.set_pos(node.pos_start, node.pos_end)
+			number, error = number.multed_by(Number(-1))
+
+
+		if error: 
+			return res.failure(error)
+		else: 
+			return res.success(number.set_pos(node.pos_start, node.pos_end))
 
 #######################################
 # RUN
@@ -402,4 +439,4 @@ def run(fn, text):
 	interpreter = Interpreter()
 	result = interpreter.visit(ast.node)
 
-	return result, None
+	return result.value, result.error
